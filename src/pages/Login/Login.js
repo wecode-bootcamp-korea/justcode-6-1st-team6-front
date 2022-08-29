824c6fc9a105a65402ff499dec370b1e570adbf6
이고운 <kowoon@igoun-ui-MacBookAir.local>
2022-08-30 01:39:33 +0900

    ADD: 로그인, 회원가입 페이지 레이아웃 생성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,64 @@
 import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import css from './Login.module.scss';
 
 function Login() {
-  return <div className={css.container}></div>;
+  const navigate = useNavigate();
+  const goToSignup = () => {
+    navigate('/signup');
+  };
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [valid, setValid] = useState(false);
+
+  const handleIdInput = e => {
+    const idValue = e.target.value;
+    setId(idValue);
+    idValue.includes('@') && pw.length >= 6 ? setValid(true) : setValid(false);
+  };
+  const handlePwInput = e => {
+    const pwValue = e.target.value;
+    setPw(pwValue);
+    id.includes('@') && pwValue.length >= 6 ? setValid(true) : setValid(false);
+  };
+
+  return (
+    <div className={css.background}>
+      <div className={css.container}>
+        <div>
+          <h1 className={css.flexCenter}>로고</h1>
+          <h2 className={css.flexCenter}>Login</h2>
+          <div>
+            <input
+              className={css.emailInput}
+              placeholder="Email address"
+              onChange={handleIdInput}
+            ></input>
+          </div>
+          <div>
+            <input
+              onChange={handlePwInput}
+              className={css.passwordInput}
+              placeholder="Password"
+            ></input>
+          </div>
+          <div className={css.ask}>
+            <span className={css.click} onClick={goToSignup}>
+              회원가입
+            </span>
+            &nbsp; 하시겠습니까?
+          </div>
+          <button
+            style={{ backgroundColor: valid ? '#9a4aeb' : '#d5b7f4' }}
+            className={`${css.loginButton} ${css.button}`}
+          >
+            로그인하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import css from './Signup.module.scss';
 
 function Signup() {
-  return <div className={css.container}></div>;
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate('/login');
+  };
+  return (
+    <div className={css.background}>
+      <div className={css.container}>
+        <div>
+          <h1>로고</h1>
+          <h2>Sign up</h2>
+          <div>
+            <input className={css.textInput} placeholder="닉네임"></input>
+          </div>
+          <div>
+            <input
+              className={css.textInput}
+              placeholder="Email address 입력"
+            ></input>
+          </div>
+          <div>
+            <input
+              className={css.textInput}
+              placeholder="Password (6자리 이상)"
+            ></input>
+          </div>
+          <div>
+            <input
+              className={css.textInput}
+              placeholder="Password 확인"
+            ></input>
+          </div>
+          <div>
+            <input type="checkbox" /> &nbsp;
+            <span className={css.click}>이용약관</span>에 동의하겠습니까?
+          </div>
+          <div className={css.ask}>
+            <span className={css.click} onClick={goToLogin}>
+              로그인
+            </span>
+            &nbsp;하시겠습니까?
+          </div>
+          <button className={css.signupButton}>회원가입하기</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
