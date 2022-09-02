4f0b0d67d18a5511b68a008737a69df06875a074
Kevin Ahn <maketheworldwise@gmail.com>
2022-09-03 03:06:16 +0900

    :sparkles: PostCardList 구현
    - Mock 데이터 추가
    - Assets 추가
    - Switch, 무한 스크롤 라이브러리 추가
    - PostCardListContainer 컴포넌트 추가
    - PostCardListCategoryItem (전체/프로젝트/스터디) 컴포넌트 추가
    - PostCardListCategory (PostCardListCategoryItem, 모집 중만 보기) 컴포넌트 추가
    - PostCardList 컴포넌트 추가

---
+++ src/components/PostCardList/PostCardList.js
@@ -0,0 +1,38 @@
+import PostCard from '../PostCard/PostCard';
+
+import styles from './PostCardList.module.scss';
+
+const CATEGORY_OPTION_ALL = '전체';
+const CATEGORY_OPTION_PROJECT = '프로젝트';
+const CATEGORY_OPTION_STUDY = '스터디';
+
+function PostCardList(props) {
+  const postCardList = props.data;
+  const categoryOption = props.categoryOption;
+  const switchOption = props.switchOption;
+
+  return (
+    <div className={styles.postCardList}>
+      {postCardList &&
+        postCardList.map(postCard => {
+          let condition = getCondition(categoryOption);
+          return switchOption
+            ? !postCard.is_closed &&
+                postCard.classification.includes(condition) && (
+                  <PostCard key={postCard.post_id} data={postCard} />
+                )
+            : postCard.classification.includes(condition) && (
+                <PostCard key={postCard.post_id} data={postCard} />
+              );
+        })}
+    </div>
+  );
+}
+
+function getCondition(option) {
+  if (option === CATEGORY_OPTION_ALL) return '';
+  if (option === CATEGORY_OPTION_STUDY) return '프로젝트';
+  if (option === CATEGORY_OPTION_PROJECT) return '스터디';
+}
+
+export default PostCardList;
